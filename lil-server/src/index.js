ec2035815e8398ebf1334db98ab49310ad6ae202
Oğuzhan Kandakoğlu <oguzhankandakoglu@gmail.com>
2021-04-30 18:39:58 +0300

    fixed : deprecation of bodyParser, added: UserSch.

--- lil-server/src/index.js
+++ lil-server/src/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
+// const bodyParser = require('body-parser'); // deprecated
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
 app.use(authRoutes);
+
 // mongoose
 const mongoUri =
   "mongodb+srv://admin:admin@cluster0.d0qey.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
