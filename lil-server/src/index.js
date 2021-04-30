c6cfb0236ef31aecf008bb307eab430a05913ade
Oğuzhan Kandakoğlu <oguzhankandakoglu@gmail.com>
2021-04-30 20:15:42 +0300

    jsonwebtoken middleware is working

--- lil-server/src/index.js
+++ lil-server/src/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 // const bodyParser = require('body-parser'); // deprecated
 const authRoutes = require("./routes/authRoutes");
+const requireAuth = require("./middlewares/requireAuth");
 
 const app = express();
 app.use(express.json());
@@ -31,8 +32,8 @@ mongoose.connection.on("error", (err) => {
 });
 // ~mongoose
 
-app.get("/", (req, res) => {
-  res.send("Hi there!");
+app.get("/", requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
--- lil-server/src/routes/authRoutes.js
+++ lil-server/src/routes/authRoutes.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
 const User = mongoose.model("User");
 
 const router = express.Router();
-router.post("/signup", (req, res) => {
-  console.log(req.body);
-  res.send("You made a post request!");
+
+router.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = new User({ email, password });
+    await user.save();
+    const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY");
+    res.send({ token }); // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MDhjMzUyN2Y5ZmUwZDBmNjg3YWEwNjEiLCJpYXQiOjE2MTk4MDEzODN9.XEykS7RmUOarB3bCJgRHgGi8lDAecok8xhs0Nsu_AsI
+  } catch (err) {
+    res.status(422).send(err.message);
+  }
 });
 
 module.exports = router;
