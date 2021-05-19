e7874580ad1acacd41b2ddea504a66a30427d70b
Oğuzhan Kandakoğlu <oguzhankandakoglu@gmail.com>
2021-05-19 14:28:22 +0300

    removed styled system

--- lil-app/App.js
+++ lil-app/App.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { SafeAreaView } from "react-native";
-import { ThemeProvider } from "styled-components";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import HomeScreen from "./src/screens/HomeScreen";
 import SearchScreen from "./src/screens/SearchScreen";
@@ -11,9 +10,7 @@ import LibraryScreen from "./src/screens/LibraryScreen";
 import SigninScreen from "./src/screens/SigninScreen";
 import SignupScreen from "./src/screens/SignupScreen";
 
-import theme from "./src/utils/theme";
 import TabBar from "./src/components/tab-bar";
-import Box from "./src/components/box";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -32,29 +29,27 @@ function mainFlow() {
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Box flex={1} as={SafeAreaView}>
-        <NavigationContainer>
-          <Stack.Navigator
-          // screenOptions={{
-          //   headerShown: false,
-          // }}
-          >
-            {isLoggedIn ? (
-              <>
-                <Stack.Screen name="Lilium" component={mainFlow} />
-                {/* <Stack.Screen name="sideFlow" component={SettingsScreen} /> */}
-              </>
-            ) : (
-              <>
-                <Stack.Screen name="Signin" component={SigninScreen} />
-                <Stack.Screen name="Signup" component={SignupScreen} />
-              </>
-            )}
-          </Stack.Navigator>
-        </NavigationContainer>
-      </Box>
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          {isLoggedIn ? (
+            <>
+              <Stack.Screen name="Lilium" component={mainFlow} />
+              {/* <Stack.Screen name="sideFlow" component={SettingsScreen} /> */}
+            </>
+          ) : (
+            <>
+              <Stack.Screen name="Signin" component={SigninScreen} />
+              <Stack.Screen name="Signup" component={SignupScreen} />
+            </>
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
