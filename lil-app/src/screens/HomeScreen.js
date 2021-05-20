ee9109a31a6955e2f4918ff5e32686b3826e8981
Oğuzhan Kandakoğlu <oguzhankandakoglu@gmail.com>
2021-05-20 10:29:07 +0300

    HomeScreen Seperator

--- lil-app/src/screens/HomeScreen.js
+++ lil-app/src/screens/HomeScreen.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { Text, StatusBar, StyleSheet, FlatList } from "react-native";
+import {
+  Text,
+  StatusBar,
+  StyleSheet,
+  FlatList,
+  ImageBackground,
+} from "react-native";
 import SafeAreaView from "react-native-safe-area-view";
 
 import theme from "../utils/theme";
@@ -11,27 +17,47 @@ import authorPic from "../../assets/authors/jamesmcbride.jpg";
 import cover1 from "../../assets/covers/cover1.jpg";
 import cover2 from "../../assets/covers/cover2.jpg";
 import cover3 from "../../assets/covers/cover3.jpg";
+import cover3Blur from "../../assets/covers/cover3Blur.jpg";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView style={[theme.styles.mainContainer, { padding: 30 }]}>
-      <Header />
+    <SafeAreaView style={[theme.styles.mainContainer]}>
+      <ImageBackground
+        source={cover3Blur}
+        style={styles.card}
+        imageStyle={styles.image}
+      >
+        <Header />
 
-      <BookCard
-        header="Deacon King Kong"
-        description="A mystery story, a crime novel, an urban farce, a sociological portrait of late-1960s Brooklyn."
-        pageCount="5200"
-        ratingCount="4.3 (1250)"
-        authorName="James McBride"
-        authorId="1"
-        authorPic={authorPic}
-        storyId="1"
-        coverPic={cover1}
-        badgeText="POPULAR"
-      />
+        <BookCard
+          header="Deacon King Kong"
+          description="A mystery story, a crime novel, an urban farce, a sociological portrait of late-1960s Brooklyn."
+          pageCount="5200"
+          ratingCount="4.3 (1250)"
+          authorName="James McBride"
+          authorId="1"
+          authorPic={authorPic}
+          storyId="1"
+          coverPic={cover1}
+          badgeText="POPULAR"
+        />
+      </ImageBackground>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  image: {
+    borderBottomLeftRadius: 60,
+  },
+  card: {
+    resizeMode: "cover",
+    top: 0,
+    padding: 30,
+    paddingBottom: 50,
+    borderBottomLeftRadius: 60,
+  },
+});
 
 export default HomeScreen;
 
